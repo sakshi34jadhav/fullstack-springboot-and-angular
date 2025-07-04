cb5a7c61984bab20fa90e92e37fa4397f019a31f
sakshi34jadhav <sakshi34jadhav@outlook.com>
2025-07-04 17:58:23 +0530

    Delete spring-blog-backend/src/main/java/com/github/sakshi34jadhav/springblog/util/AppConstant.java

--- spring-blog-backend/src/main/java/com/github/sakshi34jadhav/springblog/util/AppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.github.braians.springblog.util;
-
-public interface AppConstant {
-    String DEFAULT_PAGE_NUMBER = "0";
-    String DEFAULT_PAGE_SIZE = "20";
-    
-    String MAX_PAGE_SIZE = "50";
-}

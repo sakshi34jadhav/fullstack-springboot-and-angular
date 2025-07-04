aa796a9fad338381dcd5ed346fd4de6ec3e420d9
sakshi34jadhav <sakshi34jadhav@outlook.com>
2025-07-04 18:01:16 +0530

    Delete spring-blog-backend/src/main/java/com/github/sakshi34jadhav/springblog/service directory

--- spring-blog-backend/src/main/java/com/github/sakshi34jadhav/springblog/service/CrudService.java
+++
@@ -1,9 +0,0 @@
-package com.github.braians.springblog.service;
-
-public interface CrudService <T, P> {
-
-    T save (P p);
-    T update (Long id,P p);
-    T findById(Long id);
-    void deleteById(Long id);
-}
